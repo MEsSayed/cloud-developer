88c18295ca4fcdf740966dbd60afe86bb7808e71
MEsSayed <m.e.ismael@gmail.com>
2021-09-20 22:25:24 +0200

    master

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -1,11 +1,18 @@
 import AWS = require('aws-sdk');
 import { config } from './config/config';
 
-const c = config.dev;
+const c = config.postgres;
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.credentials = credentials;
+// ./src/aws.ts
+
+if(c.aws_profile !== "DEPLOYED") {
+  var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+  AWS.config.credentials = credentials;
+}
+
+//var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
+//AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
